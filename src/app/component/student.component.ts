20cdfebe88d9023a455a060ac323339054cfa687
Davide Amendolare <davide.amendolare@masvis.com>
2017-11-09 17:36:48 +0100

    Prove websocket

--- src/app/component/student.component.ts
+++ src/app/component/student.component.ts
@@ -4,6 +4,8 @@ import {StudentService} from '../service/student.service';
 import {StudentsSocketService} from "../service/students-socket.service";
 import {WebsocketService} from "../service/websocket.service";
 import * as electron from 'electron';
+import * as WebSocket from "ws";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 const ipcRenderer = electron.ipcRenderer;
 
 
@@ -18,10 +20,44 @@ export class StudentComponent implements OnInit {
 
   students: Student[];
   newStudent: Student;
+  form: FormGroup;
+  private wsConnection: WebSocket;
+  private wsURL: string = 'ws://localhost:8080/name';
 
-  constructor(private studentService: StudentService, private studentsSocketService: StudentsSocketService) {
-    studentsSocketService.messages.subscribe();
+  constructor(private studentService: StudentService, private fb: FormBuilder) {
+    this.wsConnect().subscribe();
+    this.form = fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      birthYear: ['', Validators.required]
+    });
   }
+
+  private wsConnect() {
+    this.wsConnection = new WebSocket(this.wsURL);
+    this.wsConnection.on('message', (evt: MessageEvent) => this.onMessage(evt));
+  }
+
+  private onMessage(evt: MessageEvent) {
+    let data = JSON.parse(evt.data);
+    this.newStudent = new Student();
+    this.newStudent.firstName = data.firstName;
+    this.newStudent.lastName = data.lastName;
+    this.newStudent.birthYear = data.birthYear;
+
+    this.students.push(this.newStudent);
+  }
+
+
+    createStudent(): void {
+      this.newStudent.firstName = this.form.value.firstName;
+      this.newStudent.lastName = this.form.value.lastName;
+      this.newStudent.birthYear = this.form.value.birthYear;
+      this.form.reset();
+      this.studentService.createStudent(this.newStudent)
+        .then(() => ipcRenderer.send('windows-student-form-close'));
+  }
+
 
   getStudents(): void {
     this.studentService
@@ -40,7 +76,7 @@ export class StudentComponent implements OnInit {
   }
 
   openForm(): void {
-    ipcRenderer.send('windows-student-form-active', this.studentsSocketService);
+    ipcRenderer.send('windows-student-form-active');
   }
 
 }
